a1b26a99369f1ab91c450b84eef4d4c79ac13fd2
Jean Carlos Soto Barbosa <jeansoto516@gmail.com>
2026-05-14 00:40:50 -0300

    feat: create user CRUD base with Spring Boot an H2

--- src/main/java/com/jean/auth_api/controller/UserController.java
+++ src/main/java/com/jean/auth_api/controller/UserController.java
@@ -6,6 +6,9 @@ import com.jean.auth_api.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -13,9 +16,27 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    //Criar usuario
     @PostMapping
     public User createUser(@RequestBody User user) {
-
         return userRepository.save(user);
+    }
+
+    //Listar todos os usuarios
+    @GetMapping
+    public List<User> getALLUsers() {
+        return userRepository.findAll();
+    }
+
+    //Buscar usuario por id
+    @GetMapping("/{id}")
+    public Optional<User> getUserById(@PathVariable Long id) {
+        return userRepository.findById(id);
+    }
+
+    //deletar usuario 
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userRepository.deleteById(id);
     }
 }
